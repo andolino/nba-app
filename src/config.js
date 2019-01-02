b4833e843128ea40921f308a5ea565949d2eb2dd
andolino <dondonpentavia@gmail.com>
2019-01-02 16:51:08 +0800

    update package, config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const CURRENT_YR = (new Date().getFullYear());
 // const URL = 'http://localhost:3006';
-const URL = 'https://andolino.github.io/nba-app/';
+const URL = 'https://andolino.github.io/nba-app';
 
 
 export {
